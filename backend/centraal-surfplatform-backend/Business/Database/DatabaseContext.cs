89e689e724a9fe7f3925739a5cbf0c3847d72968
Rick Schoenman <rickschman@hotmail.com>
2025-04-19 08:15:07 +0200

    Add SurfSpots to DbContext

--- backend/centraal-surfplatform-backend/Business/Database/DatabaseContext.cs
+++ backend/centraal-surfplatform-backend/Business/Database/DatabaseContext.cs
@@ -6,4 +6,5 @@ namespace Business.Database;
 public class DatabaseContext(DbContextOptions<DatabaseContext> options) : DbContext(options)
 {
     public DbSet<User> Users { get; set; }
+    public DbSet<SurfSpot> SurfSpots { get; set; }
 }
