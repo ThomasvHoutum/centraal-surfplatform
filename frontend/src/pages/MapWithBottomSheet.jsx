05aed0ec2c7193cf5e6a7724c3a02c7755f5ee32
David Coffa <i562945@fontysict.nl>
2025-05-05 11:03:29 +0200

    Je commit message hier

--- frontend/src/pages/MapWithBottomSheet.jsx
+++ frontend/src/pages/MapWithBottomSheet.jsx
@@ -25,8 +25,233 @@ const surfSpots = [
     waveHeight: '1,2 m',
     windDirection: 'W',
     crowd: 'Gemiddeld'
+  },
+  {
+    id: 3,
+    name: 'Wijk aan Zee',
+    position: { lat: 52.4879, lng: 4.6150 },
+    waveHeight: '1,9 m',
+    windDirection: 'NW',
+    crowd: 'Hoog'
+  },
+  {
+    id: 4,
+    name: 'Noordwijk',
+    position: { lat: 52.2447, lng: 4.4331 },
+    waveHeight: '1,4 m',
+    windDirection: 'WZW',
+    crowd: 'Gemiddeld'
+  },
+  {
+    id: 5,
+    name: 'IJmuiden',
+    position: { lat: 52.4619, lng: 4.5655 },
+    waveHeight: '1,7 m',
+    windDirection: 'NW',
+    crowd: 'Laag'
+  },
+  {
+    id: 6,
+    name: 'Brouwersdam',
+    position: { lat: 51.7769, lng: 3.8714 },
+    waveHeight: '1,3 m',
+    windDirection: 'W',
+    crowd: 'Gemiddeld'
+  },
+  {
+    id: 7,
+    name: 'Domburg',
+    position: { lat: 51.5617, lng: 3.5003 },
+    waveHeight: '2,0 m',
+    windDirection: 'WNW',
+    crowd: 'Hoog'
+  },
+  {
+    id: 8,
+    name: 'Hoek van Holland',
+    position: { lat: 51.9769, lng: 4.1167 },
+    waveHeight: '1,6 m',
+    windDirection: 'NW',
+    crowd: 'Gemiddeld'
+  },
+  {
+    id: 9,
+    name: 'Kijkduin',
+    position: { lat: 52.0639, lng: 4.2303 },
+    waveHeight: '1,5 m',
+    windDirection: 'NW',
+    crowd: 'Laag'
+  },
+  {
+    id: 10,
+    name: 'Bloemendaal aan Zee',
+    position: { lat: 52.4067, lng: 4.5364 },
+    waveHeight: '1,7 m',
+    windDirection: 'W',
+    crowd: 'Hoog'
+  },
+  {
+    id: 11,
+    name: 'Texel - Paal 9',
+    position: { lat: 53.0580, lng: 4.7162 },
+    waveHeight: '1,8 m',
+    windDirection: 'WNW',
+    crowd: 'Gemiddeld'
+  },
+  {
+    id: 12,
+    name: 'Texel - Paal 17',
+    position: { lat: 53.1112, lng: 4.7543 },
+    waveHeight: '2,1 m',
+    windDirection: 'NW',
+    crowd: 'Hoog'
+  },
+  {
+    id: 13,
+    name: 'Vlissingen',
+    position: { lat: 51.4426, lng: 3.5739 },
+    waveHeight: '1,4 m',
+    windDirection: 'ZW',
+    crowd: 'Laag'
+  },
+  {
+    id: 14,
+    name: 'Callantsoog',
+    position: { lat: 52.8236, lng: 4.6997 },
+    waveHeight: '1,5 m',
+    windDirection: 'W',
+    crowd: 'Laag'
+  },
+  {
+    id: 15,
+    name: 'Katwijk',
+    position: { lat: 52.2044, lng: 4.3984 },
+    waveHeight: '1,3 m',
+    windDirection: 'WZW',
+    crowd: 'Gemiddeld'
+  },
+  {
+    id: 16,
+    name: 'Petten',
+    position: { lat: 52.7665, lng: 4.6596 },
+    waveHeight: '1,6 m',
+    windDirection: 'NW',
+    crowd: 'Laag'
+  },
+  {
+    id: 17,
+    name: 'Julianadorp',
+    position: { lat: 52.8798, lng: 4.7484 },
+    waveHeight: '1,7 m',
+    windDirection: 'WNW',
+    crowd: 'Gemiddeld'
+  },
+  {
+    id: 18,
+    name: 'Hargen aan Zee',
+    position: { lat: 52.7107, lng: 4.6587 },
+    waveHeight: '1,5 m',
+    windDirection: 'W',
+    crowd: 'Laag'
+  },
+  {
+    id: 19,
+    name: 'Egmond aan Zee',
+    position: { lat: 52.6192, lng: 4.6185 },
+    waveHeight: '1,6 m',
+    windDirection: 'NW',
+    crowd: 'Gemiddeld'
+  },
+  {
+    id: 20,
+    name: 'Maasvlakte',
+    position: { lat: 51.9486, lng: 3.9775 },
+    waveHeight: '2,2 m',
+    windDirection: 'NW',
+    crowd: 'Gemiddeld'
+  },
+  {
+    id: 21,
+    name: 'Rockanje',
+    position: { lat: 51.8517, lng: 4.0953 },
+    waveHeight: '1,2 m',
+    windDirection: 'ZW',
+    crowd: 'Laag'
+  },
+  {
+    id: 22,
+    name: 'Velsen-Noord',
+    position: { lat: 52.4707, lng: 4.6111 },
+    waveHeight: '1,5 m',
+    windDirection: 'W',
+    crowd: 'Laag'
+  },
+  {
+    id: 23,
+    name: 'Ter Heijde',
+    position: { lat: 52.0183, lng: 4.1743 },
+    waveHeight: '1,3 m',
+    windDirection: 'NW',
+    crowd: 'Laag'
+  },
+  {
+    id: 24,
+    name: 'Ouddorp',
+    position: { lat: 51.8113, lng: 3.9363 },
+    waveHeight: '1,5 m',
+    windDirection: 'WZW',
+    crowd: 'Gemiddeld'
+  },
+  {
+    id: 25,
+    name: 'Nieuwvliet-Bad',
+    position: { lat: 51.3721, lng: 3.4884 },
+    waveHeight: '1,4 m',
+    windDirection: 'ZW',
+    crowd: 'Laag'
+  },
+  {
+    id: 26,
+    name: 'Bergen aan Zee',
+    position: { lat: 52.6707, lng: 4.6301 },
+    waveHeight: '1,6 m',
+    windDirection: 'WNW',
+    crowd: 'Gemiddeld'
+  },
+  {
+    id: 27,
+    name: 'Westenschouwen',
+    position: { lat: 51.6821, lng: 3.6942 },
+    waveHeight: '1,3 m',
+    windDirection: 'WZW',
+    crowd: 'Laag'
+  },
+  {
+    id: 28,
+    name: 'Oostvoorne',
+    position: { lat: 51.9087, lng: 4.0739 },
+    waveHeight: '1,2 m',
+    windDirection: 'ZW',
+    crowd: 'Laag'
+  },
+  {
+    id: 29,
+    name: 'Vrouwenpolder',
+    position: { lat: 51.5862, lng: 3.5762 },
+    waveHeight: '1,4 m',
+    windDirection: 'W',
+    crowd: 'Gemiddeld'
+  },
+  {
+    id: 30,
+    name: 'Texel - De Koog',
+    position: { lat: 53.0933, lng: 4.7556 },
+    waveHeight: '1,9 m',
+    windDirection: 'NW',
+    crowd: 'Hoog'
   }
 ];
+
 
 export default function MapWithBottomSheet() {
   const [selectedSpot, setSelectedSpot] = useState(null);
