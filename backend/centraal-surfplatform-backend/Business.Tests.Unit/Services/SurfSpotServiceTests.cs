2a029e65b1481a492dbf1e6f51342cb46304d2c4
Rick Schoenman <noreply@noreply.com>
2025-06-17 20:16:12 +0200

    Add comment to GetSurfSpotById_WhenIdDoesNotExist test

--- backend/centraal-surfplatform-backend/Business.Tests.Unit/Services/SurfSpotServiceTests.cs
+++ backend/centraal-surfplatform-backend/Business.Tests.Unit/Services/SurfSpotServiceTests.cs
@@ -1,4 +1,5 @@
 using Business.Services;
+using Business.Services.Models.SurfSpotService;
 using Business.Tests.Unit.Helpers;
 
 namespace Business.Tests.Unit.Services;
@@ -11,7 +12,19 @@ public class SurfSpotServiceTests
         // Arrange
         await using var context = DatabaseHelper.CreateInMemoryDbContext();
         var surfSpotService = new SurfSpotService(context);
-        const int id = 10;
+        const int id = 1;
+        
+        // note: to check if the test fails if the database does contain the requested surfspot
+        // we add a surfspot to the database
+        /*
+        var dto = new CreateSurfSpotDto
+        {
+            Name = "test",
+            Latitude = 30,
+            Longitude = 30
+        };
+        await surfSpotService.CreateSurfSpotAsync([dto]);
+        */
         
         // Act & Assert
         // Since the database is empty, any id we request, should throw an exception
